1107fbc2973a2650f4fa7602651fc4b91ea9da63
mnrazablog <mnraza94@gmail.com>
2023-11-21 11:46:30 +0530

    added tailwindcss in it

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="">
+      <h1 className='text-center py-4 text-red-600 font-bold text-3xl'>
+        Welcome to Netflix GPT
+      </h1>
+    </div>
+  );
+}
+
+export default App;

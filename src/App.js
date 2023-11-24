a1f1267cb6f4782fd99606d6f8fd3f301a069720
mnrazablog <mnraza94@gmail.com>
2023-11-24 22:44:55 +0530

    added login and register screens

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
+import Body from "./components/Body";
+
+
 function App() {
-  return (
-    <div className="">
-      <h1 className='text-center py-4 text-red-600 font-bold text-3xl'>
-        Welcome to Netflix GPT
-      </h1>
-    </div>
-  );
+  return <Body />;
 }
 
 export default App;
